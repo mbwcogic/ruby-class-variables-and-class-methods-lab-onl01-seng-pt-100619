f16446d8d3c98c83eaaaa46acc75ca4cc530905b
Michael Waldo <mbwcogic@yahoo.com>
2019-11-03 20:15:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   
   @@count=0
   @@artists = []
-  
+  @@genres = []
   
   def initialize(name, artist, genre)
    
@@ -17,7 +17,7 @@ class Song
    
    @@artists >> artist
   
-   #@@
+   #@@genres >> genre
   end
   
 end
