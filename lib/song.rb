ea316a069f5cdedf02c1a47b97de5770a0a7a940
Michael Waldo <mbwcogic@yahoo.com>
2019-11-03 21:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,16 +32,16 @@ class Song
   @@genres.uniq
   end
   
-  def self.genre_count
-  genre_count = {}
+  #def self.genre_count
+  #genre_count = {}
   
-  @@genres.each do [genre]
-     if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end 
-    end
+  #@@genres.each do [genre]
+    # if genre_count[genre]
+     #   genre_count[genre] += 1 
+      #else
+       # genre_count[genre] = 1
+      #end 
+  #  end
   genre_count
-  end
-  end
+  #end
+  #end
