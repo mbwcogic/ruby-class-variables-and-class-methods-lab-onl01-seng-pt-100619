4f33b1fc83674636eb654ce98e9cd80f882fbb58
Michael Waldo <mbwcogic@yahoo.com>
2019-11-03 20:53:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,10 @@ class Song
   genre_count = {}
   
   @@genres.each do |genre|
-    if 
+     if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
 end
 end
   
